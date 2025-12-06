9fd53dd6563ef58b9df8bed393b2977207cd41da
Nagendra <dnagendra@innoworkssoftware.com>
2025-12-06 15:04:39 +0100

    dummy test

--- src/services/webauthnService.js
+++ src/services/webauthnService.js
@@ -3,7 +3,7 @@ const {
   verifyRegistrationResponse,
   generateAuthenticationOptions,
   verifyAuthenticationResponse,
-} = require('@simplewebauthn/server');
+} = require('../simplewebauthn/server');
 const { v4: uuidv4 } = require('uuid');
 
 const storage = require('../utils/kv-storage');
